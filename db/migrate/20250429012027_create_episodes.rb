101f0b7dea00a92a5b0a867376018c59bf380a0e
Kiryuanzu <kyoka.fujiike@classi.jp>
2025-04-29 10:23:05 +0900

    episodes のテーブル定義で null:false と ユニーク制約の設定を追加

--- db/migrate/20250429012027_create_episodes.rb
+++ db/migrate/20250429012027_create_episodes.rb
@@ -1,15 +1,17 @@
 class CreateEpisodes < ActiveRecord::Migration[8.0]
   def change
     create_table :episodes do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
       t.text :summary
-      t.datetime :published_at
+      t.datetime :published_at, null: false
       t.string :duration
-      t.string :guid
+      t.string :guid, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :episodes, :guid, unique: true
   end
 end
