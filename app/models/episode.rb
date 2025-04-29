138336ec55837c1309a6d3fa0dba43c397400353
Kiryuanzu <kyoka.fujiike@classi.jp>
2025-04-29 10:23:58 +0900

    Episodeモデルでバリデーションの制約を追加

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,8 @@
 class Episode < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :description, presence: true
+  validates :published_at, presence: true
+  validates :guid, presence: true, uniqueness: true
 end
