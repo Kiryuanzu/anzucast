ba6e01681a14fb841f5e5e0cfd2e6883a43c6c7d
Kiryuanzu <kyoka.fujiike@classi.jp>
2025-04-29 16:30:22 +0900

    Admin::EpisodesController 内に index と create のアクションを追加

--- app/controllers/admin/episodes_controller.rb
+++ app/controllers/admin/episodes_controller.rb
@@ -1,2 +1,29 @@
-class Admin::EpisodesController < ApplicationController
+class Admin::EpisodesController < Admin::BaseController
+  def index
+    @episodes = Episode.order(published_at: :desc).all
+    @episode = Episode.new
+  end
+
+  def create
+    @episode = current_user.episodes.build(episode_params)
+
+    if @episode.save
+      redirect_to admin_episodes_path, notice: "エピソードを投稿しました"
+    else
+      @episodes = Episode.order(published_at: :desc)
+      render :index, status: :bad_request
+    end
+  end
+
+  private
+
+  def episode_params
+    params.require(:episode).permit(
+      :title,
+      :description,
+      :summary,
+      :published_at,
+      :duration
+    )
+  end
 end
