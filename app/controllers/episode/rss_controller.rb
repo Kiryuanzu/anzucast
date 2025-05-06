dd97cdc805ce23d6979118bc3246576614ad7ed4
Kiryuanzu <kyoka.fujiike@classi.jp>
2025-05-06 23:14:02 +0900

    rssフィードの表示順を投稿日から新しい順にする

--- app/controllers/episode/rss_controller.rb
+++ app/controllers/episode/rss_controller.rb
@@ -1,6 +1,6 @@
 class Episode::RssController < ApplicationController
     def index
-      @episodes = Episode.order(published_at: :asc)
+      @episodes = Episode.order(published_at: :desc)
 
       respond_to do |format|
         format.rss { render layout: false }
