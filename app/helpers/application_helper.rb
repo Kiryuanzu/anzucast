765965740c2cc1cb0799e965f6f6faf6c23264db
Kiryuanzu <kyoka.fujiike@classi.jp>
2025-05-30 22:59:34 +0900

    description に含まれるURLを link_to でハイパーリンク化するように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,13 @@ module ApplicationHelper
     end
     "https://#{ENV['AWS_CLOUD_FRONT_DOMAIN']}/#{attachment.key}"
   end
+
+  def description_included_url(description)
+    URI.extract(description, [ "http", "https" ]).uniq.each do |url|
+      link = link_to(url, url, target: "_blank", rel: "noopener", class: "text-blue-600 underline hover:text-blue-800")
+      description = description.gsub(url, link)
+    end
+
+    sanitize(description)
+  end
 end
